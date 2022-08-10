38bc1313b7fb5c24d3d558171d1e50b27b0ebd90
Jacob Andersson <jacob.m.andersson@gmail.com>
2022-08-10 16:22:32 +0200

    fix

--- heroku.sh
+++ heroku.sh
@@ -39,10 +39,12 @@ fi
 printf "✨ Restoring database backup to ${INPUT_HEROKU_APP_STAGING}...\n\n"
 export HEROKU_APP=${INPUT_HEROKU_APP_STAGING}
 heroku pg:backups:restore ${INPUT_HEROKU_APP_PRODUCTION}:: DATABASE_URL --confirm ${INPUT_HEROKU_APP_STAGING}
-printf "\n✅ Production database backup restored to ${INPUT_HEROKU_APP_STAGING}\n"
+printf "\n✅ Production database backup restored to ${INPUT_HEROKU_APP_STAGING}\n\n"
 
 if [ $INPUT_RAILS_MIGRATE_BLOBS = true ]; then
   # Update blobs to use staging bucket
+  echo $(which psql)
+  echo "Hmm..."
   heroku pg:psql -c "UPDATE active_storage_blobs SET service_name='staging' WHERE service_name='production';"
   printf "\n✅ Postgres: Updated service_name to staging on Active Storage blobs\n"
 fi
